5ff789ca71b580411b9b12be816bdad96fb6e57f
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-15 11:19:54 +0100

    Add crossing lines

--- paint.py
+++ paint.py
@@ -12,5 +12,12 @@ HEIGHT = 400
 my_canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg="white")
 my_canvas.pack(pady=20)
 
+# Create a line
+x1, y1, x2, y2 = 0, 100, 300, 100
+my_canvas.create_line(x1, y1, x2, y2, fill="red")
+
+# Create another line to cross the first
+x1, y1, x2, y2 = 100, 0, 100, 300
+my_canvas.create_line(x1, y1, x2, y2, fill="red")
 
 root.mainloop()
