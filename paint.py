2193090aae31eb767070f00ce6fcf2e1b4bfa4d4
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-15 15:55:57 +0100

    Change colour functionality added

--- paint.py
+++ paint.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import tkinter.ttk as ttk
+from tkinter import colorchooser
 
 root = Tk()
 root.title("Paint")
@@ -21,12 +22,13 @@ my_canvas.pack(pady=20)
 # x1, y1, x2, y2 = 100, 0, 100, 300
 # my_canvas.create_line(x1, y1, x2, y2, fill="red")
 
+brush_color = "black"
+bg_color = "white"
 
 def paint(e):
-
     # Brush params
     #brush_width = 25
-    brush_color = "green"
+
     #brush_type = BUTT
     # BUTT, ROUND, PROJECTING
 
@@ -41,7 +43,17 @@ def paint(e):
 def change_brush_size(thing):
     brush_slider_label.config(text=int(brush_slider.get()))
 
+# Change brush color
+def change_brush_color():
+    global brush_color
+    brush_color = colorchooser.askcolor(color=brush_color)[1]
+    
 
+# Change canvas background color
+def change_canvas_color():
+    global bg_color
+    bg_color = colorchooser.askcolor(color=bg_color)[1]
+    my_canvas.config(bg=bg_color)
 
 # Bind the mouse to the Canvas
 my_canvas.bind("<B1-Motion>", paint)
@@ -80,6 +92,18 @@ brush_type_radio3 = Radiobutton(brush_type_frame, text="Diamond", variable=brush
 brush_type_radio1.pack(anchor=W)
 brush_type_radio2.pack(anchor=W)
 brush_type_radio3.pack(anchor=W)
+
+# Change colors
+change_colors_frame = LabelFrame(brush_options_frame, text="Change Colors")
+change_colors_frame.grid(row=0, column=2)
+
+# Change brush color button
+brush_color_button = Button(change_colors_frame, text="Brush Color", command=change_brush_color)
+brush_color_button.pack(pady=10, padx=10)
+
+# Change canvas background color
+canvas_color_button = Button(change_colors_frame, text="Canvas Color", command=change_canvas_color)
+canvas_color_button.pack(pady=10, padx=10)
 
 
 root.mainloop()
