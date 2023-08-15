a200696924ac2e64006e3267d1b060adef080419
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-15 11:42:24 +0100

    Added brush optione

--- paint.py
+++ paint.py
@@ -12,21 +12,33 @@ HEIGHT = 400
 my_canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg="white")
 my_canvas.pack(pady=20)
 
-# Create a line
-x1, y1, x2, y2 = 0, 100, 300, 100
-my_canvas.create_line(x1, y1, x2, y2, fill="red")
+# # Create a line
+# x1, y1, x2, y2 = 0, 100, 300, 100
+# my_canvas.create_line(x1, y1, x2, y2, fill="red")
 
-# Create another line to cross the first
-x1, y1, x2, y2 = 100, 0, 100, 300
-my_canvas.create_line(x1, y1, x2, y2, fill="red")
+# # Create another line to cross the first
+# x1, y1, x2, y2 = 100, 0, 100, 300
+# my_canvas.create_line(x1, y1, x2, y2, fill="red")
 
 
 def paint(e):
+
+    # Brush params
+    brush_width = 25
+    brush_color = "green"
+    brush_type = BUTT
+    # BUTT, ROUND, PROJECTING
+
     # Create a red line following the mouse pointer
-    my_canvas.create_line(e.x, e.y, e.x+1, e.y+1, fill="red")
-    
+    my_canvas.create_line(e.x-1, e.y-1, e.x+1, e.y+1, 
+                          capstyle=brush_type, 
+                          fill=brush_color, 
+                          width=brush_width, 
+                          smooth=True)
+
 # Bind the mouse to the Canvas
 my_canvas.bind("<B1-Motion>", paint)
+
 
 
 root.mainloop()
