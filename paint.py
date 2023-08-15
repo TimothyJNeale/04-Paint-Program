5879a255bad09231960dd55fd2e7fa5cf133ca2b
TimothyJNeale <119433369+TimothyJNeale@users.noreply.github.com>
2023-08-15 11:10:30 +0100

    Added canvas

--- paint.py
+++ paint.py
@@ -1,8 +1,16 @@
 from tkinter import *
 
 root = Tk()
-root.title("Hello World!")
-root.geometry("500x500")
+root.title("Paint")
+root.geometry("800x800")
 root.iconbitmap("01 Nenebiker.ico")
+
+WIDTH = 600
+HEIGHT = 400
+
+# Create Canvas
+my_canvas = Canvas(root, width=WIDTH, height=HEIGHT, bg="white")
+my_canvas.pack(pady=20)
+
 
 root.mainloop()
